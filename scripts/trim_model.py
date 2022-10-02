4b96d5bba12b307c65df072185bc0eb119782323
sudosilico <73738695+sudosilico@users.noreply.github.com>
2022-10-02 13:57:38 -0400

    Add script to trim optimizer states to reduce model size.

---
+++ scripts/trim_model.py
@@ -0,0 +1,61 @@
+import os, sys
+import torch
+import time
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python trim_model.py <model_path>")
+        sys.exit(1)
+
+    model_path = sys.argv[1]
+
+    if not os.path.isfile(model_path):
+        print("No file was found at the given path.")
+        sys.exit(1)
+
+    start_time = time.process_time()
+
+    untrimmed_size = os.path.getsize(model_path)
+    untrimmed = torch.load(model_path, map_location="cpu")
+
+    trimmed = trim_model(untrimmed)
+
+    output_path = model_path.replace(".ckpt", "_trim.ckpt")
+    torch.save(trimmed, output_path)
+
+    end_time = time.process_time()
+    elapsed = end_time - start_time
+
+    trimmed_size = os.path.getsize(model_path)
+
+    bytes = untrimmed_size - trimmed_size
+    megabytes = bytes / 1024.0 / 1024.0
+
+    print(f"Untrimmed: {untrimmed_size} bytes.")
+    print(f"Trimmed: {trimmed_size} bytes.")
+
+    print(
+        f"\nDone! Trimmed {untrimmed_size - trimmed_size} B, or {megabytes} MB, in {elapsed} seconds."
+    )
+
+
+def trim_model(untrimmed):
+    trimmed = dict()
+
+    # before
+    for k in untrimmed.keys():
+        if k != "optimizer_states":
+            trimmed[k] = untrimmed[k]
+
+    if "global_step" in untrimmed:
+        print(f"Global step: {untrimmed['global_step']}.")
+
+    temp = trimmed["state_dict"].copy()
+
+    trimmed_model = dict()
+    for k in temp:
+        trimmed_model[k] = temp[k].half()
+    trimmed["state_dict"] = trimmed_model
+
+    return trimmed
